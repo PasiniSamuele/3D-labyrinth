0624224a14496639f9cc66cbadbd1c534c7a3eb3
Riccamper <riccardo.campi@mail.polimi.it>
2021-08-27 01:08:25 +0200

    some unuseful tests

--- resources/js/handlers/sceneHandler.js
+++ resources/js/handlers/sceneHandler.js
@@ -1,18 +1,34 @@
-function SceneHandler(movementHandler, interactionHandler){
-    this.then=0;
-    this.level;
-	this.movementHandler=movementHandler;
-	this.interactionHandler=interactionHandler;
+/*******************
+ * SceneHandler.js
+ ******************/
 
-    this.drawScene=function(now,scope, iter) {
+/**
+ * Object to manage the main rendering loop
+ */
+class SceneHandler {
 
-        // Get current time
-		now *= 0.001;  // seconds;
+	/*******************
+	 * Constructor
+	 ******************/
+
+	constructor(movementHandler, interactionHandler) {
+
+		this.then = 0;
+		this.level;
+		this.movementHandler = movementHandler;
+		this.interactionHandler = interactionHandler;
+
+	}
+
+	drawScene(now, scope) {
+
+		// Get current time
+		now *= 0.001; // seconds;
 		let deltaTime = now - scope.then;
 		scope.then = now;
 
 
-        utils.resizeCanvasToDisplaySize(gl.canvas);
+		utils.resizeCanvasToDisplaySize(gl.canvas);
 		gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 		gl.clearColor(1, 1, 1, 0);
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -21,32 +37,36 @@ function SceneHandler(movementHandler, interactionHandler){
 		gl.frontFace(gl.CCW);
 		gl.cullFace(gl.BACK);
 
+		// TODO: MAXIMUM CALL STACK EXCEEDED NON ATTRIBUIBILE AD ALCUNA DELLE FUNZIONI CHIAMATE, BENSI' AL LOOP CHIAMANTE
+		
 		scope.level.camera.idle(deltaTime);
 		let movementHandlerRet = scope.movementHandler.idle(scope.level.camera, scope.interactionHandler);
 		scope.level.camera = movementHandlerRet.camera;
 		scope.interactionHandler = movementHandlerRet.interactionHandler;
 
 
-        // Perspective, World Matrix
+		// Perspective, World Matrix
 		let perspectiveMatrix = scope.level.camera.perspectiveMatrix;
-        let viewMatrix = scope.level.camera.viewMatrix;
+		let viewMatrix = scope.level.camera.viewMatrix;
 
-        scope.level.skybox.draw(now, perspectiveMatrix, viewMatrix)
-        scope.level.labyrinth.draw(perspectiveMatrix, viewMatrix);
+		/*console.log(perspectiveMatrix);
+		console.log(viewMatrix);*/
 
-		if(iter==0)
-        	window.requestAnimationFrame(()=>scope.drawScene(now,scope,iter+1));
-    }
+		scope.level.skybox.draw(now, perspectiveMatrix, viewMatrix);
+		scope.level.labyrinth.draw(perspectiveMatrix, viewMatrix);
 
-    this.setLevel=function(level){
-        this.level=level;
-    }
-	this.start = function() {
-        this.drawScene(Date.now(),this,0);
-		
+		//this.drawScene(Date.now());
+		window.requestAnimationFrame(scope.drawScene(Date.now(), this));
+	}
+
+	setLevel(level) {
+		this.level = level;
+	}
+	start() {
+		//this.drawScene(Date.now());
+		window.requestAnimationFrame(this.drawScene(Date.now(), this));
 	}
 
 	// TODO VERIFICARE SE E' SUPERFLUO
-    //this.setLevel(level);
-    
+	//this.setLevel(level);
 }
