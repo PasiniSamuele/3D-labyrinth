1b6f9a2e80063f1984a7e6d647a99ed8e0dca985
Riccamper <riccardo.campi@mail.polimi.it>
2021-09-01 19:28:49 +0200

    texture with mtl

--- resources/js/components/mesh.js
+++ resources/js/components/mesh.js
@@ -1,11 +1,12 @@
 /**
- * shish
+ * mesh.js
  */
 
 class Mesh {
 
 	constructor(objStr) {
 		this.mesh = new OBJ.Mesh(objStr);
+		// TODO: aggiungere mtl parser migliore di quello sopra eseguito
 	}
 
 }
