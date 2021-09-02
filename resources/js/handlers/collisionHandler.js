c3925c226d2de50dc28e7da11996ec9005576889
Riccamper <riccardo.campi@mail.polimi.it>
2021-09-02 17:19:24 +0200

    new collision speed

--- resources/js/handlers/collisionHandler.js
+++ resources/js/handlers/collisionHandler.js
@@ -1,47 +1,57 @@
 class CollisionHandler {
-    constructor(){
-        this.DEBUGCOLLISION = false;
-    }
+	constructor() {
+		this.DEBUGCOLLISION = false;
+	}
 
-    checkCameraCollision(x, newx, z, newz){
-        if(!this.DEBUGCOLLISION) {
-            let zvar = newz-z;
-            let xvar = newx-x;
-            let xAbsolute = Math.floor(x+this.offset.column);
-            let zAbsolute = Math.floor(z+this.offset.row);
-            let newxAbsolute = Math.floor(newx+this.offset.column+Math.sign(xvar)*0.25);
-            let newzAbsolute = Math.floor(newz+this.offset.row+Math.sign(zvar)*0.25);
-            let minxAbsolute = Math.floor(newx+this.offset.column-Math.sign(xvar)*0.25);
-            let minzAbsolute = Math.floor(newz+this.offset.row-Math.sign(zvar)*0.25);
-            console.log(labyrinthUtils.isBlock(this.structure, newzAbsolute, newxAbsolute));
+	checkCameraCollision(x, newx, z, newz) {
 
-            
-            let cond1 = labyrinthUtils.isBlock(this.structure, newzAbsolute, newxAbsolute);
-            let cond2 = labyrinthUtils.isBlock(this.structure, zAbsolute, newxAbsolute);
-            let cond3 = labyrinthUtils.isBlock(this.structure, newzAbsolute, xAbsolute);
-            let cond4 = labyrinthUtils.isBlock(this.structure, newzAbsolute, minxAbsolute);
-            let cond5 = labyrinthUtils.isBlock(this.structure, minzAbsolute, newxAbsolute);
+		let xCollide = false, zCollide = false;
+		
+		if (!this.DEBUGCOLLISION) {
+			let zvar = newz - z;
+			let xvar = newx - x;
+			let xAbsolute = Math.floor(x + this.offset.column);
+			let zAbsolute = Math.floor(z + this.offset.row);
+			//TODO: AGGIUNGERE PARAMETRI IN SETTINGS
+			let newxAbsolute = Math.floor(newx + this.offset.column + Math.sign(xvar) * 0.175);
+			let newzAbsolute = Math.floor(newz + this.offset.row + Math.sign(zvar) * 0.175);
+			let minxAbsolute = Math.floor(newx + this.offset.column - Math.sign(xvar) * 0.175);
+			let minzAbsolute = Math.floor(newz + this.offset.row - Math.sign(zvar) * 0.175);
 
-            if(xAbsolute != newxAbsolute && (cond2 || cond1 && !cond2 && !cond3 || !cond1 && cond5)){
-                newx = x;
-            } //else if (xAbsolute != newxAbsolute && labyrinthUtils.isBlock(this.structure, ))
-            if(zAbsolute != newzAbsolute && (cond3 || cond1 && !cond2 && !cond3 || !cond1 && cond4)){
-                newz = z;
-            }
-        }
+			let cond1 = labyrinthUtils.isBlock(this.structure, newzAbsolute, newxAbsolute);
+			let cond2 = labyrinthUtils.isBlock(this.structure, zAbsolute, newxAbsolute);
+			let cond3 = labyrinthUtils.isBlock(this.structure, newzAbsolute, xAbsolute);
+			let cond4 = labyrinthUtils.isBlock(this.structure, newzAbsolute, minxAbsolute);
+			let cond5 = labyrinthUtils.isBlock(this.structure, minzAbsolute, newxAbsolute);
 
-        return {
-            x:newx,
-            z:newz
-        };
-    }
+			if (xAbsolute != newxAbsolute && (cond2 || cond1 && !cond2 && !cond3 || !cond1 && cond5)) {
+				newx = x;
+				xCollide = true;
+			} //else if (xAbsolute != newxAbsolute && labyrinthUtils.isBlock(this.structure, ))
+			if (zAbsolute != newzAbsolute && (cond3 || cond1 && !cond2 && !cond3 || !cond1 && cond4)) {
+				newz = z;
+				zCollide = true;
+			}
+		}
 
-    setStructure(str){
-        this.structure = str;
-        let start = labyrinthUtils.computeStartPos(str);
-        this.offset = {
-            row: start[0]+0.5,
-            column: start[1]+0.5
-        };
-    }
+		return {
+			position: {
+				x: newx,
+				z: newz
+			},
+			collide: {
+				x: xCollide,
+				z: zCollide
+			}
+		};
+	}
+
+	setStructure(str) {
+		this.structure = str;
+		let start = labyrinthUtils.computeStartPos(str);
+		this.offset = {
+			row: start[0] + 0.5,
+			column: start[1] + 0.5
+		};
+	}
 }
