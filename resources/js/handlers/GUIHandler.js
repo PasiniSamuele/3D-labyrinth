685fbb8e6e75ac4c20bf3568a70fe628405a8756
riccamper <riccardo.campi@mail.polimi.it>
2021-09-04 18:59:43 +0200

    gui

---
+++ resources/js/handlers/GUIHandler.js
@@ -0,0 +1,36 @@
+/*******************
+ * GUIHandler.js
+ ******************/
+
+class GUIHandler {
+
+	/**
+	 * Constructor
+	 */
+	 constructor() {
+
+		// Commands options
+		this.menu = [
+			{
+				name: "Play Game",
+				func: function() {},
+				//visible: function(param) { return param },
+				child: []
+			},
+			{
+				name: "Options",
+				func: function() {},
+				child: []
+			},
+			{
+				name: "Play Game",
+				func: function() {},
+				child: []
+			}
+		];
+		
+	}
+
+
+
+}
