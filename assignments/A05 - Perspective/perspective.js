50ab897444c05e3b3d48165f6696d5cbe11dca62
teobia98 <matteo18.bianchi@mail.polimi.it>
2021-09-25 15:54:01 +0200

    assignment corrections

--- assignments/A05 - Perspective/perspective.js
+++ assignments/A05 - Perspective/perspective.js
@@ -12,7 +12,7 @@ function perspective() {
 	var O1 = getPersMatrix(90, 4/3, 1, 101);
 
 	// Make perspective projection, l = -1.2, r = 0, t = 0.3375, b = -0.3375, n = 1, f = 101. Note: due to the asimmetry of this projection, only the left part of the scene should be visible
-	var O2 = getPersMatrix(-1.2, 0, 0.3375, -0.3375, 1, 101);
+	var O2 = getPersMatrix2(-1.2, 0, 0.3375, -0.3375, 1, 101);
 
 	return [A1, A2, A3, O1, O2];
 }
@@ -25,7 +25,7 @@ function getPersMatrix(fov,a,n,f){
 		0.0,		0.0,		-1.0,		0.0];
 }
 
-function getPersMatrix(l,r,t,b,n,f){
+function getPersMatrix2(l,r,t,b,n,f){
 	return [2*n/(r-l),	0.0,		(r+l)/(r-l),		0.0,
 		0.0,		2*n/(t-b),		(t+b)/(t-b),		0.0,
 		0.0,		0.0,		(f+n)/(n-f),		2*f*n/(n-f),
