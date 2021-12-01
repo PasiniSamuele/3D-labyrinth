6a4647da5738b5f23e6c414318fba3232d10056a
teobia98 <matteo18.bianchi@mail.polimi.it>
2021-12-01 13:21:06 +0100

    added ao & updated character

--- resources/js/components/pbrLabyrinth.js
+++ resources/js/components/pbrLabyrinth.js
@@ -37,6 +37,36 @@ class PbrLabyrinth {
 		this.children.push(new Pedestal(this.structure2D, this, this.programs[3], pedestalStr[0], pedestalStr[1]));
 
 		this.children.forEach(child => child.init());
+		
+		
+		//OBJ maker for labyrinth
+		let s = "";
+		for(let i = 0; i < this.children[0].vertices.length; i += 3){
+			s += "v " + this.children[0].vertices[i] + " " + this.children[0].vertices[i+1] + " " + this.children[0].vertices[i+2] + "\n";
+		}
+		for(let i = 0; i < this.children[1].vertices.length; i += 3){
+			s += "v " + this.children[1].vertices[i] + " " + this.children[1].vertices[i+1] + " " + this.children[1].vertices[i+2] + "\n";
+		}
+		for(let i = 0; i < this.children[0].normals.length; i += 3){
+			s += "vn " + this.children[0].normals[i] + " " + this.children[0].normals[i+1] + " " + this.children[0].normals[i+2] + "\n";
+		}
+		for(let i = 0; i < this.children[1].normals.length; i += 3){
+			s += "vn " + this.children[1].normals[i] + " " + this.children[1].normals[i+1] + " " + this.children[1].normals[i+2] + "\n";
+		}
+		for(let i = 0; i < this.children[0].uvs.length; i += 2){
+			s += "vt " + this.children[0].uvs[i] + " " + this.children[0].uvs[i+1] + "\n";
+		}
+		for(let i = 0; i < this.children[1].uvs.length; i += 2){
+			s += "vt " + this.children[1].uvs[i] + " " + this.children[1].uvs[i+1] + "\n";
+		}
+		for(let i = 0; i < this.children[0].indices.length; i += 3){
+			s += "f " + (this.children[0].indices[i]+1) + " " + (this.children[0].indices[i+1]+1) + " " + (this.children[0].indices[i+2]+1) + "\n";
+		}
+		for(let i = 0; i < this.children[1].indices.length; i += 3){
+			s += "f " + ((this.children[1].indices[i] + this.children[0].vertices.length/3)+1) + " " + ((this.children[1].indices[i+1]+ this.children[0].vertices.length/3)+1) + " " + ((this.children[1].indices[i+2]+ this.children[0].vertices.length/3)+1) + "\n";
+		}
+		console.log(s)
+		
 	}
 
 	/**
